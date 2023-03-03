9841c8b3f456ea15893216410f98f173c5d66601
Marco Haber <marcoshaber99@gmail.com>
2023-03-03 20:39:03 +0200

    left at 30:35

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
 
 function App() {
+
+   //state to determine what page we are on
+   const[selectedPage,setSelectedPage] = useState("home")
+   const isAboveMediumScreens = useMediaQuery("(min-width:1060px");
+
   return (
-    <div className="">
+
+    <div className="app">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       
     </div>
   );
